3f2d4c24f41ee593f5ac2912ba58692b2fbcd472
ClothexNepal <clothex91@gmail.com>
2024-05-20 17:38:01 +0545

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,20 +1,23 @@
 <?php
-//get data from form  
+// Get data from form
 $name = $_POST['name'];
-$email= $_POST['email'];
-$message= $_POST['message'];
-$size= $_POST['option'];
-$quantity= $_POST['quantity'];
-$phone= $_POST['phone'];
-$item= $_POST['item'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$size = $_POST['option'];
+$quantity = $_POST['quantity'];
+$phone = $_POST['phone'];
+$item = $_POST['item'];
 $to = "clothex91@mail.com";
 $subject = "Mail From Clothex website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message. "\r\n size=" .$size. "\r\n quantity" .$quantity. "\r\n phone=" .$phone. "\r\n item=" .$item; 
+$txt = "Name: $name \r\nEmail: $email \r\nMessage: $message \r\nSize: $size \r\nQuantity: $quantity \r\nPhone: $phone \r\nItem: $item";
 $headers = "From: noreply@yoursite.com" . "\r\n" .
-"CC: somebodyelse@example.com";
-if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
+    "CC: somebodyelse@example.com";
+
+// Attempt to send email
+if (mail($to, $subject, $txt, $headers)) {
+    header("Location: thankyou.html");
+    exit;
+} else {
+    echo "Failed to send email. Please try again later.";
 }
-//redirect
-header("Location:thankyou.html");
 ?>
